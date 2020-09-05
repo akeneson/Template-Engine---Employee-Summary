6a6c73e5303270c45d201bcac32643d8f2a9f0c6
Amy Keneson <AmyKeneson@amys-imac.attlocal.net>
2020-09-04 17:12:42 -0700

    employee class added

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Employee class
+
+// OOP module 18: Ins-Classes
+
+class Employee{
+    constructor(name, email, id){
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+    // function to get employees name
+    printInfo(){
+        console.log(`Name: ${this.name}`);
+        console.log(`email: ${this.email}`);
+        console.log(`ID: ${this.id}`);
+    }
+}
+
+const employee = new Employee("ak", "ak@gmail.com", "0312");
+
+employee.printInfo();
