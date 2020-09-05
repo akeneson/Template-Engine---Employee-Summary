67af110e95dc0070c437870935b235dd3c143062
Amy Keneson <AmyKeneson@amys-imac.attlocal.net>
2020-09-05 18:53:31 -0500

    addEngineer() | addIntern()

--- app.js
+++ app.js
@@ -10,145 +10,159 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const { stringify } = require("querystring");
-
+const { endianness } = require("os");
+// creates an empty array of employees to push to.
 const employeeList = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// use the inquirer npm package to prompt user for their email, id, and specific information based on their role with the company.
-const questions = [
-    {
-        type: "input",
-        name: "enteredName",
-        message: "Enter your name: "
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Enter your e-mail: "
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Enter your ID: "
-    },
-    {
-        type: "list",
-        name: "jobRole",
-        message: "Select your role.",
-        choices: [
-            "Manager",
-            "Engineer",
-            "Intern",
-            "Not listed"
-        ]
-    }
-];
-
+// *****To add a manager*****
 function addManager() {
     inquirer.prompt([{
         type: "input",
         name: "managerName",
-        message: "Enter your anager name: "
+        message: "Enter the Manager's name: "
     },
     {
         type: "input",
         name: "managerEmail",
-        message: "Enter your e-mail: "
+        message: "Enter the Manager's e-mail: "
     },
     {
         type: "input",
         name: "managerID",
-        message: "Enter your ID: "
+        message: "Enter the Manager's ID: "
     },
     {
         type: "input",
         name: "phoneNumber",
-        message: "Enter Manger's phone number"
+        message: "Enter the Manger's phone number"
     }
     ]).then(answers => {
         let manager = new Manager(answers.managerName, answers.managerEmail, answers.managerID, answers.phoneNumber);
         employeeList.push(manager);
-        console.log(employeeList);
         buildTeam();
     })
 }
 
+
+// *****To add a Engineer*****
+function addEngineer() {
+    console.log(employeeList);
+    inquirer.prompt([{
+        type: "input",
+        name: "engineerName",
+        message: "Enter the Engineer's name: "
+    },
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "Enter the Engineer's e-mail: "
+    },
+    {
+        type: "input",
+        name: "engineerID",
+        message: "Enter the Engineer's ID: "
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter the Engineer's github username"
+    }
+    ]).then(answers => {
+        let engineer = new Engineer(answers.engineerName, answers.engineerEmail, answers.engineerID, answers.github);
+        employeeList.push(engineer);
+        buildTeam();
+    })
+}
+
+function addIntern() {
+    inquirer.prompt([{
+        type: "input",
+        name: "internName",
+        message: "Enter the Intern's name: "
+    },
+    {
+        type: "input",
+        name: "internEmail",
+        message: "Enter the Intern's e-mail: "
+    },
+    {
+        type: "input",
+        name: "InternID",
+        message: "Enter the Intern's ID: "
+    },
+    {
+        type: "input",
+        name: "School",
+        message: "Enter the school's name that the Intern is associated with: "
+    }
+    ]).then(answers => {
+        let intern = new Intern(answers.internName, answers.internEmail, answers.InternID, answers.school);
+        buildTeam();
+    })
+    console.log(employeeList);
+
+}
+
+
+
+function end(){
+    console.log("End.");
+}
+
+
+// This functions prompts user to choose the type of employee they want to add and call the appropriate functions of prompts.
 function createTeam() {
     inquirer.prompt([{
         type: "list",
-        name: "jobRole",
-        message: "Choose employee job Role",
+        message: "Choose the employee's job Role",
         choices: [
             "Manager",
             "Engineer",
             "Intern",
             "Not Listed"
-        ]
+        ],
+        name: "jobRole"
     }]).then(answers => {
-        switch (answers.jobRole) {
-            case "Manager":
-                addManager();
-                break;
-            default: 
-                buildTeam();
+        if (answers.jobRole === "Manager") {
+            addManager();
         }
+        else if (answers.jobRole === "Engineer") {
+            addEngineer();
+        }
+        else if (answers.jobRole === "Intern") {
+            addIntern();
+        }
+        else
+            end();
+        // switch(answers.jobRole){
+        //     case "Engineer":
+        //         addEngineer();
+        //         break;
+        //     default:
+        //         buildTeam();
+        // }
     })
 }
 
-// This function prompts the array of questions
-function promptQuestions() {
-    return inquirer.prompt(questions);
-}
-
-// function addEmployee(answers) {
-//     if (answers.jobRole === "Manager") {
-//         console.log("This employee's role is a Manager");
-//         const Manager = new Manager(answers.name, answers.email, answers.id);
-//         return Manager;
-//     }
-//     else if (answers.jobRole === "Engineer") {
-//         console.log("This employee's  is an Engineer");
-//         const Engineer = new Engineer(answers.name, answers.email, answers.id);
-//         return Engineer;
-//     }
-//     else if (answers.jobRole === "Intern") {
-//         console.log("This employee's  is an Intern");
-//         const Intern = new Intern(answers.name, answers.email, answers.id);
-//         return Intern;
-//     }
-//     else {
-//         console.log("This employee's role is not listed");
-//     }
-// }
-
 function buildTeam() {
+    // if statement checks if there is an exisiting filles prior to writing a the file
     if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR);
     }
+    // call 'render' function and passing an array (employeeList) containing all employee objects
     fs.writeFileSync(outputPath, render(employeeList), "UTF-8");
+    // createTeam() initiates prompt to add an employee
     createTeam();
     console.log(employeeList);
 }
+
+// calling function to write file
 buildTeam();
-// // This function initializes the program
-// async function init() {
-//     try {
-//         console.log("Function init called");
-//         const storeData = await promptQuestions().then(answers => {
-//             console.log(answers);
-//             addEmployee(answers)
-//         })
-//     }
-//     catch (err) {
-//         return console.log(err);
-//     }
-// }
 
-// // call to initialize program
-// init();
-
+// *****************Instructions*****************
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+// use the inquirer npm package to prompt user for their email, id, and specific information based on their role with the company.
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
